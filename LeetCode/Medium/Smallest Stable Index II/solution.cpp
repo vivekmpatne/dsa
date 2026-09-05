b8569fb31abb5ed06faac5bea09657c9d403d1d3
Vivek Patne <vivekpatnem@gmail.com>
2026-09-05 08:31:23 +0530

    [Syncode] LeetCode/Medium/Smallest Stable Index II

---
+++ LeetCode/Medium/Smallest Stable Index II/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ansIdx = 0;
+
+        int glbMax = INT_MIN;
+        int ansMax = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            glbMax = max(glbMax, nums[i]);
+
+            if( i == ansIdx) ansMax = max(ansMax, nums[i]);
+
+            if(nums[i] < ansMax - k){
+                ansIdx = i + 1;
+                ansMax = glbMax;
+            }
+        }
+
+        return ansIdx < n ? ansIdx : -1;
+
+    }
+};
