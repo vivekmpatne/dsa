fe025c97a877a5f37cc9af079c9727f33f7a20b0
Vivek Patne <vivekpatnem@gmail.com>
2026-09-21 22:23:40 +0530

    [Syncode] LeetCode/Easy/Max Consecutive Ones

---
+++ LeetCode/Easy/Max Consecutive Ones/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt_one = 0, maxi = 0;
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == 1){
+                cnt_one++;
+                maxi = max(cnt_one, maxi);
+            }else{
+                cnt_one = 0;
+            }
+        }
+        return maxi;
+    }
+};
